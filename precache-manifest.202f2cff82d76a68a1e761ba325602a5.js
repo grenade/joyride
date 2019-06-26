47c551c0341f18b84a9d6f491cb8ec4aaaa219a2
grenade <rthijssen@gmail.com>
2019-06-26 18:24:54 +0300

    Updates

---
+++ precache-manifest.202f2cff82d76a68a1e761ba325602a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7a30e528ff1261ac6619a5878f42bc",
+    "url": "/joyride/index.html"
+  },
+  {
+    "revision": "84dcd776b466c9c350d6",
+    "url": "/joyride/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/joyride/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "84dcd776b466c9c350d6",
+    "url": "/joyride/static/js/main.1c583ca1.chunk.js"
+  },
+  {
+    "revision": "b980fec3fdb322a9cc05",
+    "url": "/joyride/static/js/runtime~main.35e491b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joyride/static/media/logo.5d5d9eef.svg"
+  }
+]);
